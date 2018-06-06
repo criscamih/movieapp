c6df1e4eae87a5bf2c815d0ac5d9ce61423cbcc9
Cristian Sosa <ing.criscamih7@gmail.com>
2018-06-05 22:47:18 -0500

    creando vista y assets junto

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(document).ready(()=>{
+	const OMDB_KEY="4455de9"
+	const OMDB_URL="http://www.omdbapi.com/?apikey="+OMDB_KEY+"&"
+
+	$('#form').on('submit',getMovieByName);
+
+	function getMovieByName(event)
+	{	event.preventDefault()
+		let pelicula=$('#search-text').val()
+		getMovie(pelicula)
+	}
+
+	getMovie=(pelicula)=>{
+		axios.get(OMDB_URL+'s='+pelicula)
+			.then((response)=>{
+				console.log(response.data.Search)
+				let movies=response.data.Search;
+				renderMovies(movies)
+			})
+			.catch((err)=>{
+				console.log(err)
+			})
+	}
+
+	renderMovies=(movies)=>{
+		let output='';
+		$.each(movies,(index,movie)=>{
+			output+=`<div class="col-md-3  mb-3 text-center ">
+						<div class="well p-3">
+							<img src=${movie.Poster}>
+							<h5>${movie.Title}</h5>
+							<a href="" class="btn btn-primary" onclick="getMovieById('${movie.imdbID}')">
+								Ver Detalle
+							</a>
+						</div>
+					</div>`
+		})
+		$('#movies').html(output)
+		
+	}
+
+
+})
